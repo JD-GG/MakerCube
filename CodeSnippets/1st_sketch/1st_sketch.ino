aff4dfe6f72aac0496b46a08f446e575602ef5e7
Peparani <aaron.reiber2005@gmail.com>
2025-02-19 01:09:55 +0100

    Uart Method! & LEDs switching colors

--- CodeSnippets/1st_sketch/1st_sketch.ino
+++ CodeSnippets/1st_sketch/1st_sketch.ino
@@ -11,12 +11,20 @@ bool doAnimation = false;
 RgbColor red(colorSaturation, 0, 0);
 RgbColor green(0, colorSaturation, 0);
 RgbColor blue(0, 0, colorSaturation);
+RgbColor yellow(colorSaturation, colorSaturation, 0);    // Gelb (Rot + Grün)
+RgbColor cyan(0, colorSaturation, colorSaturation);      // Cyan (Grün + Blau)
+RgbColor magenta(colorSaturation, 0, colorSaturation);   // Magenta (Rot + Blau)
+RgbColor orange(colorSaturation, colorSaturation / 2, 0); // Orange (Mehr Rot als Grün)
+RgbColor pink(colorSaturation, colorSaturation / 3, colorSaturation / 2); // Pink (Rot + wenig Blau)
+RgbColor lime(colorSaturation / 2, colorSaturation, 0);  // Limettengrün (Mehr Grün als Rot)
+RgbColor turquoise(0, colorSaturation / 2, colorSaturation); // Türkis (Mehr Blau als Grün)
+RgbColor violet(colorSaturation / 2, 0, colorSaturation); // Violett (Mehr Blau als Rot)
 RgbColor white(colorSaturation);
 RgbColor black(0,0,0);
 
 #define DELAYVAL 500
 
-NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount,PixelPin);
+NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart1800KbpsMethod> strip(PixelCount,PixelPin);
 
 void setup(){
   strip.Begin();
@@ -31,12 +39,18 @@ void clear(){
 }
 
 void loop() {
-  clear();
+  RgbColor colors[] = { red, blue, green, yellow, cyan, magenta, 
+                        orange, pink, lime, turquoise, violet, 
+                        white, black };
 
-  for(int i=0; i<NUMPIXELS; i++) {
+  int numColors = sizeof(colors) / sizeof(colors[0]); // Anzahl der Farben
 
-    strip.SetPixelColor(i, red);
-    strip.Show();
-    delay(DELAYVAL);
+  for (int i = 0; i < numColors; i++) { 
+    for (int j = 0; j < NUMPIXELS; j++) { // LEDs nacheinander leuchten lassen
+      clear(); // Vorherige LEDs ausschalten
+      strip.SetPixelColor(j, colors[i]);  
+      strip.Show();
+      delay(DELAYVAL);
+    }
   }
 }
